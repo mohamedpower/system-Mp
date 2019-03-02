3718ea3153932c7e78a7fa651ecddb9059b474ef
mohamedpower <44884883+mohamedpower@users.noreply.github.com>
2019-03-03 01:52:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 const getYoutubeID = require('get-youtube-id');
 const fetchVideoInfo = require('youtube-info');
 const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const discord_token = "NTM1MTAxNzEzNTg2MTkyMzk0.DyDQNw.3XQu7IRlrl9-UmpFBWd_PMmkRx8";
+const discord_token = "NTUwMzQyODg2MDc2NzEwOTEy.D1hCag.L4cc4Znaf-jBQ0RdRj03mFbmSgY";
 var table = require('table').table
 var prefix = "-";
 
